eb676a29e46d1085507ddf7bc5ae789cde3a3ce3
Andrew Toussaint <ajntoussaint@gmail.com>
2023-03-21 17:26:34 -0400

    fix: spelling error

--- src/components/Bekenstein.component.js
+++ src/components/Bekenstein.component.js
@@ -13,7 +13,7 @@ export default function Bekenstein(props){
             <div className='projectDescription'>
                 <h3>Project Overview</h3>
                 <p><a href="https://note-taking-app-jofz.onrender.com/profile">Bekenstein Limit</a> is a note taking app desgined to help the user organize their thoughts on various 
-                subjects. Notes can be organized into folders and given tags. Filters can then be applied to easily find the note you are looking for. I was inspired to create 
+                subjects. The name is inspired by physicist Jacob Bekenseins theoretical limit of the maximum information that can be stored in a physical location. Notes can be organized into folders and given tags. Filters can then be applied to easily find the note you are looking for. I was inspired to create 
                 this app as I found many of the exisitng apps that tackle this problem to be to bulky for basic needs. I personally use it to organize my software engineering notes
                 and it has proven much easier than flipping through paper copies. The frontend uses Pug although if I have the chance to redo the app I would use react instead. The
                 backend is Node.js and express with a mongoDB Atlas database storing the notes. The application is hosted on render.com. Go check it out!</p>
