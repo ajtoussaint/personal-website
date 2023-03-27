7af8403c0077851fe82b67fab8738aa113d6b752
Andrew Toussaint <ajntoussaint@gmail.com>
2023-03-27 18:25:10 -0400

    chore: updated together cars link

--- src/components/TogetherCars.component.js
+++ src/components/TogetherCars.component.js
@@ -8,12 +8,12 @@ import EC2 from '../images/EC2.png'
 export default function TogetherCars(props){
     return(
         <div className="pageWrapper">
-            <a href="http://3.132.211.26/">
+            <a href="https://togethercars.dev/">
                 <img src={TCimg} alt="Together Cars" className="projectImg"></img>
             </a>
             <div className='projectDescription'>
                 <h3>Project Overview</h3>
-                <p><a href="http://3.132.211.26/">Together Cars</a> is a web app developed using a React frontend and a Node.js/Express
+                <p><a href="https://togethercars.dev/">Together Cars</a> is a web app developed using a React frontend and a Node.js/Express
                 backend with mongoDB Atlas as the database. The purpose of the app is to aid in coordinating ride sharing between friends. Users can log in to their profile, create trips, invite other users, mange who will
                 be driving and who will be a passenger in which car and share trips by link. The app is currently live on an AWS EC2 server so go check it out and start using cars together! </p>
             </div>
